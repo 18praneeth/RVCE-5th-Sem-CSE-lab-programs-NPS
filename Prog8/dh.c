2ec9c01aee841ffc062e05feeb9604671d626dd4
VenjanV <82959024+VenjanV@users.noreply.github.com>
2022-03-16 13:34:51 +0530

    updated dh.c

--- Prog8/dh.c
+++ Prog8/dh.c
@@ -3,30 +3,25 @@
 #include<time.h>
 #include<math.h>
 
-
-// a^m mod p
-long long int compute(long long int a, long long int m, long long int p)
-{
- return(((long long int) pow(a,m))%p);
-}
-
-
-
 int main()
 {
-	long long int p = 23;		// modulus
-	long long int g = 5;		// base
-
-	long long int a, b;	// a - Alice's Secret Key, b - Bob's Secret Key.
+	long long int p,g;
+	long long int a, b;	// a - Alice's private Key, b - Bob's private Key.
 	long long int A, B;	// A - Alice's Public Key, B - Bob's Public Key
+	printf("Enter the prime no(p) :");
+	scanf("%lld",&p);
+	printf("\nEnter the primitive root of p(g) :");
+	scanf("%lld",&g);
+	printf("\nEnter the alice private key (a) :");
+	scanf("%lld",&a);
+	printf("\nEnter the Bob private key (b) :");
+	scanf("%lld",&b);
+	
+	A = (long long int) pow(g,a)%p;
+	B = (long long int) pow(g,b)%p;
 
-	a = 4;
-	A = compute(g, a, p);
-	b = 3;
-	B = compute(g, b, p);
-
-	long long int keyA = compute(B, a, p);
-	long long int keyB = compute(A, b, p);
+	long long int keyA = (long long int) pow(B,a)%p;
+	long long int keyB = (long long int) pow(A,b)%p;
 
 	printf("\nAlice's Secret Key is %lld\nBob's Secret Key is %lld\n\n", keyA, keyB);
 
